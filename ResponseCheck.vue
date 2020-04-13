c300f6bcb5f91336a4de1932b504a6876b1586a0
jym <cym19921224@gmail.com>
2020-04-13 18:51:36 +0900

    heroku init

--- ResponseCheck.vue
+++ ResponseCheck.vue
@@ -1,21 +1,71 @@
 <template>
   <div>
-    <div id="screen">{{ message }}</div>
-    <div>평균 시간:{{}}</div>
-    <button @click="onReset">리셋</button>
+    <div id="screen" :class="state" @click="onClickScreen">{{ message }}</div>
+    <div>평균 시간:{{ result[result.length - 1] }}</div>
+    <button @click.prevent="onReset">리셋</button>
   </div>
 </template>
 
 <script>
+let startTime = 0;
+let endTime = 0;
+let timeout = null;
 export default {
   data() {
     return {
       result: [],
-      state: 'wating',
+      state: 'waiting',
       message: '클릭해서 시작하세요.'
     };
+  },
+  methods: {
+    onReset() {
+      console.log('cliked');
+      this.result = [];
+    },
+    onClickScreen() {
+      if (this.state === 'waiting') {
+        this.state = 'ready';
+        this.message = '초록색이되면 클릭하세요';
+        timeout = setTimeout(() => {
+          this.state = 'now';
+          this.message = '지금클릭';
+          startTime = new Date();
+        }, Math.floor(Math.random() * 1000 + 2000)); //2~3초
+      } else if (this.state === 'ready') {
+        clearTimeout(timeout);
+        this.message = '너무 성급하시군요';
+        this.state = 'now';
+      } else if (this.state === 'now') {
+        endTime = new Date();
+
+        this.state = 'waiting';
+        this.message = '클릭해서 시작하세요';
+        this.result.push(endTime - startTime);
+      }
+    }
   }
 };
 </script>
 
-<style></style>
+<style scoped>
+#screen {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  user-select: none;
+}
+
+#screen.waiting {
+  background-color: aqua;
+}
+
+#screen.ready {
+  background-color: red;
+  color: white;
+}
+
+#screen.now {
+  background-color: greenyellow;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,18 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader' //loader말고 use로해도됨
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'] //loader는 js가 아닌애들을 js로 바꿔주는역할
       }
     ]
   },
-  plugins: [new VueLoaderPlugin()],
+  plugins: [new VueLoaderPlugin()], // loader의 부가적인 역할
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist'
   }
 };
 
